8e5fdaeddeffcd69c38a1c5c7ad8ead4482a801a
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-19 19:24:40 +0530

    Add new paragraph

---
+++ looping.cpp
@@ -0,0 +1,13 @@
+//write thus program print number from 1 ton .How will we write a code for this problem?
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+    cout<<i<<"  "<<endl;
+    i=i+1;
+    return 0;
+  }
+  }
