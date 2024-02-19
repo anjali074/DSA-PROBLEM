f17647a633916d1b25b5cf4ebc7043e42afd626f
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-19 18:24:29 +0530

    Add new paragraph

---
+++ condition3.cpp
@@ -0,0 +1,18 @@
+//Q- write the program which will tell us wheather the number is postive , negaative or Zero
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  cin>>a;
+  if(a>0){
+    cout<<"the number of a is postive"<<endl;
+  }
+  else if(a<0){
+    cout<<" the number of a a is negative "<<endl;
+  }
+    else{
+      cout<<"the number  of  a is zero"<<endl;
+      return 0;
+    }
+  }
+
