22ad2f5855b207d33b9df5ffe6805785db7fea72
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-19 18:16:43 +0530

    Add new paragraph

---
+++ condition2.cpp
@@ -0,0 +1,18 @@
+//write the program print positive if is positive and else is not 
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  cin>>a;
+  //if a is positive
+  if(a>0){
+    cout<<"A is positive "<<endl;
+  }
+  else{
+    cout<<"B is negative"<<endl;
+return 0;
+  }
+
+    }
+
+  
