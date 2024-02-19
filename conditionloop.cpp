a3e6115e70726ed6d0476e418314987306bb1975
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-19 18:08:47 +0530

    Add new paragraph

---
+++ conditionloop.cpp
@@ -0,0 +1,17 @@
+///write the program A is bigger then b ,else print 
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;//help takin g the input
+  cout<<"enter the value of a and b"<<endl;
+if(a>b){//check condition
+  cout<<"value A is bigger"<<endl;
+   if(b>a){
+    cout<<"B is bigger"<<endl;
+
+  }
+}
+
+}
+
