a4cfeb141e319dba066c1eb75fadcf6cd8db2b52
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-19 17:55:33 +0530

    Add new paragraph

---
+++ operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=2;
+int b=3;
+bool first =(a==b);
+  cout<<first<<endl;
+  bool second =(a>b);
+  cout<<second<<endl;
+  bool third =(a<b);
+  cout<<third<<endl;
+  bool fourth =(a==0);
+  cout<<fourth<<endl;
+}
